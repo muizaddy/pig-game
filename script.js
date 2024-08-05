dfa386907aff6c88523de6b4c7dfcb0da0be0b23
Muizaddy <muizaddy@gmail.com>
2024-08-05 08:25:14 +0100

    I added a game info

--- script.js
+++ script.js
@@ -81,3 +81,34 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', init);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelector('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+  btnOpenModal.addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key)
+
+  if(e.key === 'Escape') closeModal()
+
+});
+
